651cdc94f4434fee0a6ff89c8139bb6e21eeaf28
Alex Clarke <alex-clarke13@live.co.uk>
2014-05-15 15:20:58 +0100

    Singleton Pattern
    - Added static initialisation singleton
    - Folder name changes

---
+++ Singleton Pattern/Singleton Pattern/Singleton_Eager_Instantiation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    class Singleton_Eager_Instantiation
+    {
+
+        //Eager instantiation of singleton
+        private static Singleton_Eager_Instantiation uniqueInstance = new Singleton_Eager_Instantiation();
+
+        //Private constructor
+        private Singleton_Eager_Instantiation() { }
+
+        public static Singleton_Eager_Instantiation getInstance()
+        {
+            return uniqueInstance;
+        }
+    }
+}
